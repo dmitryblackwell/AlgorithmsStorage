f0dd18833221797ba93dc488f2260cf3a40d2016
dmitryblackwell <dmitryblackwell@gmail.com>
2018-04-13 13:39:43 +0300

    ShellSort
    Add ShellSort and run some tests

--- src/com/blackwell/arraysorts/Demo.java
+++ src/com/blackwell/arraysorts/Demo.java
@@ -3,18 +3,19 @@ package com.blackwell.arraysorts;
 import java.util.Random;
 
 public class Demo {
-    private static final int ARR_SIZE=10_000;
+    private static final int ARR_SIZE=100_000_000;
 
     public static void main(String[] args) {
         int[] arr = new int[ARR_SIZE];
         Random R = new Random();
         for(int i =0;i<ARR_SIZE;++i)
             arr[i] = R.nextInt(1000);
-        BubbleSort.main(arr);
-        CombSort.main(arr);
+        BubbleSort.main(arr);   //7
+        CombSort.main(arr);   //5
+        ShellSort.main(arr);   //3
         InsertionSort.main(arr);
-        QuickSort.main(arr);
-        SelectionSort.main(arr);
+        QuickSort.main(arr);   //4
+        SelectionSort.main(arr);   //6
         ShakerSort.main(arr);
     }
 }
